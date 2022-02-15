472241b5f74a9f8b922d1fecd441786c3f1b0298
Jack Wilson <jackwilson@Jacks-MacBook-Pro-2.local>
2022-02-15 10:27:51 -0600

    Tried catapillar asci, made cool pattern

--- 10Print.js
+++ 10Print.js
@@ -6,12 +6,14 @@ function draw () {
   setTimeout(draw,1000)
   let output = ''
   for (let j = 0; j < w; j++) {
-
+    output += '_/-\\_%'
+/*
     if (Math.random() > 0.5){
       output += '\\'
     } else {
       output += '/'
     }
+    */
   }
   console.log(output)
 }
