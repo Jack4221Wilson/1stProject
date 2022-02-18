09152425749fb20925ec83c5b447f017c3864d04
Jack Wilson <jackwilson@Jacks-MacBook-Pro-2.local>
2022-02-18 13:57:45 -0600

    First attempt to make random length shapes on each line

---
+++ mathTest.js
@@ -0,0 +1,25 @@
+
+let maxLength = 30
+let loop = 5
+let output = ''
+
+fucntion draw(){
+  setTimeout(draw,500)
+  for (let i = 0; i > loop; i++) {
+    let ran = Math.floor(Math.random() * 11)
+    output += '['
+    for (let i = 0; i > ran; i++){
+      output += '-'
+    }
+    output += ']'
+  }
+  if (output.length >= maxLength){
+    let sub = output.length - maxLength - 1
+    for (let i = 0; i > sub; i++) {
+      output.pop()
+    }
+    output += ']'
+  }
+  console.log(output)
+}
+draw()
